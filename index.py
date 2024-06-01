fadd1b242b6c7965a9d21cc07aa5dc30b45c0f7c
akuzadsc <122011594+akuzadsc@users.noreply.github.com>
2024-06-01 22:41:37 +0200

    Add files via upload

---
+++ index.py
@@ -0,0 +1,85 @@
+import discord
+from discord.ext import commands
+from datetime import datetime
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix='!', intents=intents)
+
+ROLE_ID = 1245877322721263616  
+CHANNEL_ID = 1246000024874123345  
+LOG_CHANNEL_ID = 1246547597259440179  
+BL_ROLE_ID = 1246544747041128562 
+
+@client.event
+async def on_ready():
+    print("""
+ ____        _     _____          _     _
+|  _ \      | |   |  __ \        | |   | |
+| |_) | ___ | |_  | |__) | __ ___| |_  | |
+|  _ < / _ \| __| |  ___/ '__/ _ \ __| | |
+| |_) | (_) | |_  | |   | | |  __/ |_  |_|
+|____/ \___/ \__| |_|   |_|  \___|\__| (_)
+    """)
+
+@client.event
+async def on_member_update(before, after):
+    role = after.guild.get_role(ROLE_ID)
+    if role is not None:
+        if "❌" in before.display_name and "❌" not in after.display_name:
+            if role in before.roles:
+                await after.remove_roles(role)
+                message = f"{after.mention} a perdu le rôle {role.name}"
+                print(message)
+        elif "❌" not in before.display_name and "❌" not in after.display_name:
+            if role in after.roles:
+                await after.remove_roles(role)
+                message = f"{after.mention} a perdu le rôle {role.name}"
+                print(message)
+
+@client.event
+async def on_message(message):
+    if not message.author.bot and client.user in message.mentions:
+        member = message.author
+        if "❌" in member.display_name:
+            if message.channel.id == CHANNEL_ID and not ("@everyone" in message.content or "@here" in message.content):
+                bl_role = message.guild.get_role(BL_ROLE_ID)
+                role = message.guild.get_role(ROLE_ID)
+                if role is not None:
+                    if bl_role in member.roles:
+                        reply_msg = f"{member.mention}, je ne peux pas vous donner le rôle {role.name} car vous avez le rôle {bl_role.name} !"
+                    elif role in member.roles:
+                        reply_msg = f"{member.mention}, vous avez déjà le rôle {role.name} !"
+                    else:
+                        await member.add_roles(role)
+                        reply_msg = f'{member.mention}, le rôle "{role.name}" vous a été donné avec succès !'
+
+                        log_embed = discord.Embed(title="Nouveau Log Autorank !", color=discord.Color.from_rgb(25, 25, 25))
+                        log_embed.add_field(name="Modérateur", value="<@BOT_ID>")
+                        log_embed.add_field(name="Membre", value=f"{member.mention}")
+                        log_embed.add_field(name="Rôle donné", value=f"{role.mention}")
+                        log_embed.set_footer(text=f"Donné le {datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')}")
+
+                        log_channel = client.get_channel(LOG_CHANNEL_ID)
+                        if log_channel is not None:
+                            await log_channel.send(embed=log_embed)
+                        else:
+                            reply_msg = f"Une erreur est survenue, veuillez contacter un admin !"
+                else:
+                    reply_msg = f"Une erreur est survenue, veuillez contacter un admin !"
+            else:
+                reply_msg = f"{member.mention}, vous devez mentionner le bot dans le canal {client.get_channel(CHANNEL_ID).mention} pour recevoir le rôle !"
+        else:
+            role = message.guild.get_role(ROLE_ID)
+            if role is not None and role in member.roles:
+                await member.remove_roles(role)
+                reply_msg = f"{member.mention}, Vous avez perdu le rôle {role.name} car vous n'avez plus le tag !"
+            else:
+                reply_msg = f"{member.mention}, Vous devez avoir le tag pour obtenir le rôle !"
+
+        bot_message = await message.channel.send(reply_msg)
+
+    await client.process_commands(message)
+
+client.run('MTI0NjE4NDAwMzc1MzQ4MDI3NQ.G27Z-o.xRt2voWP5leZB_XG7A14sXh1jYF9oAvTAk58kQ')
